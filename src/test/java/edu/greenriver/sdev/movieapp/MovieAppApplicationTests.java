3e2ac799b425e9ca2778e34caa8474e4967fee38
joshuanakatani <joshua.nakatani@gmail.com>
2024-03-14 13:46:35 -0700

    Fixed!

--- src/test/java/edu/greenriver/sdev/movieapp/MovieAppApplicationTests.java
+++ src/test/java/edu/greenriver/sdev/movieapp/MovieAppApplicationTests.java
@@ -37,4 +37,25 @@ class MovieAppApplicationTests
         assertNotNull(movies);
         assertTrue(movies.length > 0);
     }
+
+    @Test
+    public void addMovieTest() {
+        String url = "http://localhost:" + port + "/api/v1/movies";
+
+        Movie movie = new Movie(
+                0, "Dune #2", 2024, "Science Fiction", "5 stars", true
+        );
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity request = new HttpEntity(movie, headers);
+
+        ResponseEntity<Movie> response = rest.exchange(url, HttpMethod.POST, request, Movie.class);
+
+        Movie addedMovie = response.getBody();
+
+        assertEquals(response.getStatusCode(), HttpStatus.OK);
+        assertNotNull(addedMovie);
+        assertTrue(addedMovie.getId() > 0);
+    }
 }
